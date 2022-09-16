9277ea65a69975048da91c084538958b0e5a2abb
McGrath <Michael.McGrath@k202domain.com>
2022-09-16 09:55:26 -0230

    Initial commit

---
+++ src/FunWithOperators.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Scanner;
+
+
+
+public class FunWithOperators {
+
+    public static void main(String[] args) {
+        System.out.println("Fun with Operators");
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        System.out.println("Enter second number");
+    }
+}
